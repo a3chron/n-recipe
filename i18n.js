5d56957eaded93f2e2a7b7964ee7b2c2e5d1eedd
a3chron <kurt.schambach@gmail.com>
2025-09-19 11:09:37 +0200

    feat: update translations

--- i18n.js
+++ i18n.js
@@ -2,8 +2,11 @@ import * as Localization from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import de from "./locales/de.json";
+import de from "./locales/de/en.json";
+import esPE from "./locales/en-PE/en.json";
 import en from "./locales/en.json";
+import it from "./locales/it/en.json";
+import sk from "./locales/sk/en.json";
 
 // Safely get locale code, defaulting to 'en'
 const locales = Localization.getLocales();
@@ -17,6 +20,9 @@ i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     de: { translation: de },
+    es: { translation: esPE },
+    it: { translation: it },
+    sk: { translation: sk },
   },
   interpolation: {
     escapeValue: false,
