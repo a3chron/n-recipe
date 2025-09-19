a416a4fd2f27819e3a122fd9c93a38135d4e0103
a3chron <kurt.schambach@gmail.com>
2025-09-19 11:11:51 +0200

    feat: improve translations

--- i18n.js
+++ i18n.js
@@ -2,16 +2,15 @@ import * as Localization from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import de from "./locales/de/en.json";
-import esPE from "./locales/en-PE/en.json";
+import de from "./locales/de.json";
 import en from "./locales/en.json";
-import it from "./locales/it/en.json";
-import sk from "./locales/sk/en.json";
+import esPE from "./locales/es-PE.json";
+import it from "./locales/it.json";
+import sk from "./locales/sk.json";
 
-// Safely get locale code, defaulting to 'en'
 const locales = Localization.getLocales();
 const localeCode =
-  locales && locales.length > 0 ? locales[0].languageCode : "en";
+  locales && locales.length > 0 ? locales[0].languageTag : "en";
 
 i18n.use(initReactI18next).init({
   compatibilityJSON: "v3",
@@ -20,13 +19,11 @@ i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     de: { translation: de },
-    es: { translation: esPE },
+    "es-PE": { translation: esPE },
     it: { translation: it },
     sk: { translation: sk },
   },
-  interpolation: {
-    escapeValue: false,
-  },
+  interpolation: { escapeValue: false },
 });
 
 export default i18n;
